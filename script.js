b514cbc10ec87f71f5bbb50f3b78c89b74e276f9
kartikchaurasiya296-sketch <kartikchaurasiya296@gmail.com>
2026-08-17 09:19:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const toggle=document.querySelector('.menu-toggle');const nav=document.querySelector('.nav');if(toggle&&nav){toggle.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('.nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));}
